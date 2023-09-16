f94ac70e7d5475bbbe2b22c07025ecf7b52630fe
astr0n0mer <imran2000786@gmail.com>
2023-09-17 00:21:36 +0530

    Create the initial draft of Tip calculator app challenge from FEM

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# tip-calculator-app
-Tip calculator app challenge from Frontend Mentor only using HTML, CSS and JavaScript
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+| Mobile                              | Desktop                              |
+| ----------------------------------- | ------------------------------------ |
+| ![](./images/screenshot-mobile.png) | ![](./images/screenshot-desktop.png) |
+
+### Links
+
+- [Solution](./)
+- [Live Site](https://astr0n0mer.github.io/tip-calculator-app/index.html)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+## Author
+
+- Portfolio - [Imran Khan](https://imrank.vercel.app/)
+- Github - [astr0n0mer](https://www.github.com/astr0n0mer)
+- Frontend Mentor - [astr0n0mer](https://www.frontendmentor.io/profile/astr0n0mer)
+- Twitter - [imrank404](https://twitter.com/imrank404)
---
+++ app.js
@@ -0,0 +1,87 @@
+const form = document.querySelector(".js-form");
+const billInput = document.querySelector(".js-bill");
+const tipButtons = document.querySelectorAll(".js-tip-button");
+const customTipInput = document.querySelector(".js-custom-tip");
+const numberOfPeopleInput = document.querySelector(".js-number-of-people");
+
+const tipPerPersonLabel = document.querySelector(".js-tip-per-person");
+const totalPerPersonLabel = document.querySelector(".js-total-per-person");
+
+const validateInput = (element, errorQuery, min) => {
+  const value = element.valueAsNumber;
+
+  let error = "";
+  if (isNaN(value)) error = "Invalid number";
+  if (min >= 0 && value < 0) error = "Can't be negative";
+  if (min > 0 && value === 0) error = "Can't be 0";
+
+  const errorElement = document.querySelector(errorQuery);
+  if (error) {
+    errorElement.innerText = error;
+    errorElement.classList.add("js-error--active");
+    throw new Error(error);
+  } else {
+    errorElement.classList.remove("js-error--active");
+  }
+};
+
+const clearTipButtons = () =>
+  tipButtons.forEach((tipButton) => (tipButton.checked = false));
+
+const calculateTotalBillAndTip = () => {
+  const bill = billInput.valueAsNumber;
+  const tipPercentage =
+    parseInt(form.elements.tip.value) || customTipInput.valueAsNumber;
+  const numberOfPeople = numberOfPeopleInput.valueAsNumber;
+
+  if (isNaN(bill) || isNaN(tipPercentage) || isNaN(numberOfPeople)) return;
+
+  const tipAmount = bill * (tipPercentage / 100);
+  const totalAmount = bill + tipAmount;
+  const tipPerPerson = tipAmount / numberOfPeople;
+  const totalPerPerson = totalAmount / numberOfPeople;
+
+  setInnerText(tipPerPersonLabel, tipPerPerson);
+  setInnerText(totalPerPersonLabel, totalPerPerson);
+};
+
+const setInnerText = (element, text) => {
+  const formatOptions = {
+    currency: "USD",
+    maximumFractionDigits: "2",
+    style: "currency",
+  };
+
+  element.innerText = new Intl.NumberFormat("en-US", formatOptions).format(
+    text
+  );
+};
+
+billInput.addEventListener("keyup", () => {
+  validateInput(billInput, ".js-bill-error", 0);
+  calculateTotalBillAndTip();
+});
+
+tipButtons.forEach((tipButton) =>
+  tipButton.addEventListener("click", () => {
+    document
+      .querySelector(".js-tip-error")
+      .classList.remove("js-error--active");
+    calculateTotalBillAndTip();
+  })
+);
+
+customTipInput.addEventListener("keyup", () => {
+  validateInput(customTipInput, ".js-tip-error", 0);
+  calculateTotalBillAndTip();
+});
+
+customTipInput.addEventListener("focus", () => {
+  clearTipButtons();
+  customTipInput.dispatchEvent("keyup");
+});
+
+numberOfPeopleInput.addEventListener("keyup", () => {
+  validateInput(numberOfPeopleInput, ".js-number-of-people-error", 1);
+  calculateTotalBillAndTip();
+});
